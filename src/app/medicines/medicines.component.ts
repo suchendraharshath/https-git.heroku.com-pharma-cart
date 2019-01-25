60442544a4c82f80c1991cbc371bd6c0c3df1e10
suchendra harshath <suchendraharshath@gmail.com>
2019-01-25 11:17:34 +0530

    make it better

--- src/app/medicines/medicines.component.ts
+++ src/app/medicines/medicines.component.ts
@@ -11,9 +11,9 @@ import { Observable } from 'rxjs';
 export class MedicinesComponent implements OnInit {
 
   constructor(private ms:MedicinesService, private http:HttpClient) {   }
-  data:object[]=[];
-  data1:object[]=[];
-  data2:object[]=[];
+  data:any[]=[];
+  data1:any[]=[];
+  data2:any[]=[];
   category:string;
   medicine:string;
   price:string;
